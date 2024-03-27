157ee545d4f8fc2306855f6e6fbec13c51684af8
nidovic <feutseludovic@gmail.com>
2024-03-27 13:35:24 +0100

    Enhance import for disbursement. (#17)
    * Enhance import for disbursement.
    
    * Enhance import for disbursement.
    
    * Update version.

--- orange_money/src/index.ts
+++ orange_money/src/index.ts
@@ -1,6 +1,7 @@
 export { Disbursement } from './disbursements/implementations/disbursement_service';
 export { DisbursementXTargetEnvironmentType } from './disbursements/utils/constants';
 export { DisbursementInterface } from './disbursements/implementations/disbursement_service';
+export { DisbursementStatus } from './disbursements/utils/status';
 export {
   OrangeMoneyPayment,
   OrangeMoneyPaymentInterface,
