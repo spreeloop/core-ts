9ead6f492a5680d13a1e3b8ea10276fe9d0e2d39
nidovic <feutseludovic@gmail.com>
2026-04-27 16:40:14 +0100

    Add storage client implementation. (#51)

--- storage/src/implementations/fake/fake.ts
+++ storage/src/implementations/fake/fake.ts
@@ -7,10 +7,15 @@ export class FakeStorage implements Storage {
   /**
    * Generates a non-expiring download URL for files stored in your buckets.
    * Anyone with this URL can permanently access the file.
+   * @param {string} filePath The file path.
    * @return {string} The download url.
    */
-  async generateDownloadURL(link?: string | null | undefined): Promise<GenerateDownloadURLResponse> {
-    const url = link ? link : 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
+  async generateDownloadURL(
+    filePath?: string | null | undefined
+  ): Promise<GenerateDownloadURLResponse> {
+    const url = filePath
+      ? filePath
+      : 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
     return { data: url, raw: url };
   }
 }
